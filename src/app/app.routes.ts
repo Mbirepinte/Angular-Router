565f689fc4a8fb500b69734007861835c76727c6
Mbirepinte <mickael.birepinte@gmail.com>
2023-02-20 21:20:54 +0100

    router ok

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+import { SignupComponent } from './signup/signup.component';
+
+import { UserComponent } from './user/user.component';
+
+
+const ROUTES: Routes = [
+
+  { path: 'sign-up', component: SignupComponent },
+
+  { path: 'user', component: UserComponent },
+
+  { path: '', component: UserComponent },
+
+];
+
+
+export { ROUTES };
