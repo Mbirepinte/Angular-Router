54a5f34c393de8b6cced42bcbc99280327ed02c4
Mbirepinte <mickael.birepinte@gmail.com>
2023-02-21 14:37:35 +0100

    form ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { SignupComponent } from './signup/signup.component';
@@ -17,7 +17,8 @@ import { ROUTES } from './app.routes';
    ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES), 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
+
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  model : User = new User("","","","");
+
+  constructor() { 
+  }
 
   ngOnInit() {
   }
-
+  onSubmit() :void {
+    console.log(this.model);
+  }
 }
